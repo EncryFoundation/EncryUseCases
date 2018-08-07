c11dd843b22a1f70afe892a8ca27fc4b54bdc0a5
Ilya Oskin <ilya.arcadich@gmail.com>
2018-08-07 18:37:41 +0300

    Atomic swap (#1)
    * Contract for Ethereum added
    
    * sha256 is used in order to achieve compatibility with Encry
    
    * Initiator's contract in Prism
    
    * Comlete contract
    
    * gitignore
    
    * Removal
    
    * Removal
    
    * Readme fix
    
    * Readme fixed
    
    * Readme fixed

---
+++ atomic_swap/Readme.md
@@ -0,0 +1,11 @@
+# Atomic swap (with Ethereum)
+
+Atomic Swap is a trade between two users of different cryptocurrencies, wich does not require third party to be involved.
+Cross-chain swaps require each party to pay into a contract transaction, one contract for each blockchain.
+The contracts contain an output that is spendable by either party, but the rules required for redemption are different for each party involved.
+
+Illustration for the steps each party performs and the transfer of data between each party.
+
+<img src="img/workflow.svg" width="100%" height=650 />
+
+Detailed algorithm description [Decred](https://github.com/decred/atomicswap)
---
+++ atomic_swap/eth/AtomicSwap.sol
@@ -0,0 +1,137 @@
+pragma solidity ^0.4.24;
+
+contract AtomicSwap {
+
+    enum State { 
+        Empty,
+        Initiator,
+        Participant
+    }
+
+    struct Swap {
+        uint startTimestamp;
+        uint redemptionPeriod;
+        bytes32 hashedSecret;
+        bytes32 secret;
+        address initiator;
+        address participant;
+        uint256 value;
+        bool emptied;
+        State state;
+    }
+
+    mapping(bytes32 => Swap) public swaps;
+
+    event Refunded(uint _refundTime);
+    event Redeemed(uint _redeemTime);
+    event Participated(
+        address _initiator, 
+        address _participant,
+        bytes32 _hashedSecret,
+        uint256 _value
+    );
+    event Initiated(
+        uint _initTimestamp,
+        uint _refundTime,
+        bytes32 _hashedSecret,
+        address _participant,
+        address _initiator,
+        uint256 _funds
+    );
+
+    constructor () public {}
+
+    modifier isRefundable(bytes32 _hashedSecret) {
+        require(now > swaps[_hashedSecret].startTimestamp + swaps[_hashedSecret].redemptionPeriod);
+        require(swaps[_hashedSecret].emptied == false);
+        _;
+    }
+
+    modifier isRedeemable(bytes32 _hashedSecret, bytes32 _secret) {
+        require(sha256(abi.encodePacked(_secret)) == _hashedSecret);
+        require(now > swaps[_hashedSecret].startTimestamp + swaps[_hashedSecret].redemptionPeriod);
+        require(swaps[_hashedSecret].emptied == false);
+        _;
+    }
+
+    modifier isInitiator(bytes32 _hashedSecret) {
+        require(msg.sender == swaps[_hashedSecret].initiator);
+        _;
+    }
+
+    modifier isNotInitiated(bytes32 _hashedSecret) {
+        require(swaps[_hashedSecret].state == State.Empty);
+        _;
+    }
+
+    function initiate(uint _refundPeriod, bytes32 _hashedSecret, address _participant) 
+        external
+        payable
+        isNotInitiated(_hashedSecret)
+    {
+        swaps[_hashedSecret].redemptionPeriod = _refundPeriod;
+        swaps[_hashedSecret].startTimestamp = now;
+        swaps[_hashedSecret].hashedSecret = _hashedSecret;
+        swaps[_hashedSecret].participant = _participant;
+        swaps[_hashedSecret].initiator = msg.sender;
+        swaps[_hashedSecret].state = State.Initiator;
+        swaps[_hashedSecret].value = msg.value;
+        emit Initiated(
+            swaps[_hashedSecret].startTimestamp,
+            _refundPeriod, 
+            _hashedSecret,
+            _participant,
+            msg.sender,
+            msg.value
+        );
+    }
+
+    function participate(uint _refundPeriod, bytes32 _hashedSecret, address _initiator)
+        external
+        payable
+        isNotInitiated(_hashedSecret)
+    {
+        swaps[_hashedSecret].redemptionPeriod = _refundPeriod;
+        swaps[_hashedSecret].startTimestamp = now;
+        swaps[_hashedSecret].participant = msg.sender;
+        swaps[_hashedSecret].initiator = _initiator;
+        swaps[_hashedSecret].value = msg.value;
+        swaps[_hashedSecret].hashedSecret = _hashedSecret;
+        swaps[_hashedSecret].state = State.Participant;
+        emit Participated(
+            _initiator,
+            msg.sender,
+            _hashedSecret,
+            msg.value
+        );
+    }
+
+    function redeem(bytes32 _secret, bytes32 _hashedSecret)
+        external
+        isRedeemable(_hashedSecret, _secret)
+    {
+        if (swaps[_hashedSecret].state == State.Participant) {
+            swaps[_hashedSecret].initiator.transfer(swaps[_hashedSecret].value);
+        }
+        if (swaps[_hashedSecret].state == State.Initiator) {
+            swaps[_hashedSecret].participant.transfer(swaps[_hashedSecret].value);
+        }
+        swaps[_hashedSecret].emptied = true;
+        swaps[_hashedSecret].secret = _secret;
+        emit Redeemed(now);
+    }
+
+    function refund(bytes32 _hashedSecret)
+        external
+        isRefundable(_hashedSecret)
+    {
+        if (swaps[_hashedSecret].state == State.Participant) {
+            swaps[_hashedSecret].participant.transfer(swaps[_hashedSecret].value);
+        }
+        if (swaps[_hashedSecret].state == State.Initiator) {
+            swaps[_hashedSecret].initiator.transfer(swaps[_hashedSecret].value);
+        }
+        swaps[_hashedSecret].emptied = true;
+        emit Refunded(now);
+    }
+}
